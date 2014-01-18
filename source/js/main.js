418ee9d52f145a0f3905235e5ab972b9d200bbba
James Hong <hh2473@columbia.edu>
2014-01-17 20:01:45 -0500

    added contact page, modified css

--- source/js/main.js
+++ source/js/main.js
@@ -1,1 +1,17 @@
+// $(document).ready(function () {
+//   if (window.outerWidth < 386) {
+// 		$('#brand').html('CU LiNK');
+// 	}
+// 	else {
+// 		$('#brand').html('CU Liberty in North Korea');
+// 	}
+// });
 
+// window.onresize = function() {
+// 	if (window.outerWidth < 386) {
+// 		$('#brand').html('CU LiNK');
+// 	}
+// 	else {
+// 		$('#brand').html('CU Liberty in North Korea');
+// 	}
+// }

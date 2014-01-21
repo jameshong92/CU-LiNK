ed9ae1ed59939f7201b68b48642137f03b7079a8
James Hong <hh2473@columbia.edu>
2014-01-21 09:52:26 -0500

    added news page

--- source/js/news.js
+++ source/js/news.js
@@ -1,3 +1,6 @@
+var months = [" ", "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+var categories = ["shared_story", "mobile_status_update", "wall_post", "published_story"];
+
 window.fbAsyncInit = function() {
   FB.init({
     appId      : '199641883439198',
@@ -17,30 +20,61 @@ window.fbAsyncInit = function() {
 
 var getPosts = function() {
   FB.api(
-      "156461214445282/posts?access_token=CAACEdEose0cBAD11mbMZBwYQ0oYzdnAB7QrkyVxKH3yCzvMksbxTl2SvkETvOyTpaC4vIZBZBo7oDN0fp7YckTvOUDq9HbDPqGZC0NcCgBOnnMOZCZCzyvBjAYZBLz0Mm2RIcZAwyoAL3lSYlG3I1qHPGNW1WX85NCZBRKytiHZBBaGQfUzUFSS8F1NJ9pw6m7jnhmJbYVcbffiQZDZD",
+      "156461214445282/posts?access_token=199641883439198|vek5DCKZitLd9raLUiT0rT7guhM",
       function (response) {
-        console.dir(response);
-        // $('#photos').html('');
-        // try {
-        //   console.dir(response);
-        //   var data = response.data;
-        //   if (data.length < 1)
-        //     throw err;
-        //   // displaying 24 recent photos
-        //   var $row = '<div class="row">';
-        //   var limit = data.length > 23 ? 24 : data.length;
-        //   for (var count = data.length-1; count >= data.length-limit; count--) {
-        //       var link = '<div class="col-sm-3"><div class="image" style="background-image: url(' + data[count].source + ')"></div></div>';
-        //       $row = $row + link;
-        //   }
-        //   $row = $row + '</div>';
-        //   $('#photos').append($row);
-        // }
-        // catch(err) {
-        //   console.dir("Error: no photos to display");
-        //   var $row = '<div class="row"><p><b>There are no photos to display.</b></p></div>';
-        //   $('#photos').append($row);
-        // }
+        $('#news').html('');
+        try {
+          console.dir(response);
+          var data = response.data;
+          if (data.length < 1)
+            throw err;
+          // displaying 24 recent news
+          var $row = '<div class="row">';
+          var limit = data.length > 24 ? 24 : data.length;
+          var columnCount = 0;
+          for (var count = 0; count < limit; count++) {
+            if (typeof data[count].status_type != "undefined" && $.inArray(data[count].status_type, categories) > -1) {
+              var picture = '../img/logos/link_logo.jpg';
+              if (typeof data[count].picture != "undefined") {
+                var index = data[count].picture.indexOf('url=');
+                if (index > -1) {
+                  picture = data[count].picture.substring(index+4).replace(/%3A/g, ':');
+                  picture = picture.replace(/%2F/g, '/');
+                }
+                else
+                  picture = data[count].picture;
+              }
+              var message = typeof data[count].message != "undefined" ? data[count].message : "No content";
+              message = message.split("\n").join("<br>").replace(/<br><br>/g, "<br>");
+              if (message.length > 140) {
+                message = message.substring(0, 137) + '...';
+              }
+              var time = data[count].created_time.split('T')[0];
+
+              var year  = time.split("-")[0];
+              var month   = months[time.split("-")[1]];
+              var day = time.split("-")[2];
+
+              var link = 'http://www.facebook.com/' + data[count].id.split("_").join("/posts/");
+              var news = '<div class="col-sm-3"><a href="' + link + '" target="_blank"><div class="image"><img src=' + picture + '></div><div class="text"><h4>' + month + ' ' + day + ', ' + year + '</h4><p>' + message + '</p></div></a></div>';
+              $row = $row + news;
+              columnCount++;
+            }
+            else
+              limit -= 1;
+            if (columnCount > 3) {
+              $row = $row + '</div>';
+              $('#news').append($row);
+              $row = '<div class="row">';
+              columnCount = 0;
+            }
+          }
+        }
+        catch(err) {
+          console.dir(err + "Error: no news to display");
+          var $row = '<div class="row"><p><b>There are no news to display.</b></p></div>';
+          $('#news').append($row);
+        }
       }
   );
 }
